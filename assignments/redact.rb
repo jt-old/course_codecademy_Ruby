c0bc7de77a07a1f29a478c7277c403dd0a627354
justintsugranes <justintsugranes@proton.me>
2022-09-29 07:32:07 -0400

    add challenges to redact example

--- assignments/redact.rb
+++ assignments/redact.rb
@@ -25,3 +25,9 @@ else
   print word + " "
 end
 }
+
+=begin What could you do to make sure your redactor redacts a word regardless of whether it’s upper case or lower case?
+
+How could you make your program take multiple, separate words to REDACT?
+
+How might you make a new redacted string and save it as a variable, rather than just printing it to the console? =end
