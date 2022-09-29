10c073d00eb81ecb4be27c50c5d947150424db59
justintsugranes <justintsugranes@proton.me>
2022-09-29 06:43:39 -0400

    add while loop example

---
+++ while_looping.rb
@@ -0,0 +1,10 @@
+# Looping with "while"
+
+# declare variable i and starting point for loop
+i = 1
+while i < 51 do
+  # print current value of i
+  print i
+  # increment count of i by 1
+  i += 1
+end
