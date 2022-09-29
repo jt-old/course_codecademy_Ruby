2fb058751136278d8193fc4dbed76095b30fd9c9
justintsugranes <justintsugranes@proton.me>
2022-09-29 06:46:49 -0400

    add looping using until example

---
+++ assignments/looping_until.rb
@@ -0,0 +1,10 @@
+# Looping with "until"
+
+# declare variable j and starting point for loop
+j = 1
+until j == 51 do
+# print current value of j
+  print j
+  # increment count of j by 1
+  j+= 1
+end
