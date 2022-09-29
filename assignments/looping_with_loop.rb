d22c66d49b2a7129af26528fce9ca65ec46e04a8
justintsugranes <justintsugranes@proton.me>
2022-09-29 06:53:43 -0400

    add looping with "loop" example

---
+++ assignments/looping_with_loop.rb
@@ -0,0 +1,13 @@
+# Looping the loop with "Loop"
+
+# declare a variable (i) to count
+i = 0
+# declare loop
+loop do
+  # increment variable (i)
+  i += 1
+  # print variable (i)
+  print "Squishy!"
+  # break the loop if count of i = 30
+  break if i == 30
+end
