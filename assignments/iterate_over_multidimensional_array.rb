258559edfdbd8abdae26bbe48a9c10e8716b9fcf
justintsugranes <justintsugranes@proton.me>
2022-09-30 07:11:15 -0400

    add iterate over multidimensional array example

---
+++ assignments/iterate_over_multidimensional_array.rb
@@ -0,0 +1,14 @@
+# declare array
+s = [["ham", "swiss"], ["turkey", "cheddar"], ["roast beef", "gruyere"]]
+
+# iterate over each element of s array through a do loop
+# store elements in sub_array
+s.each do |sub_array|
+  # iterate through each element in sub array through a do loop
+  sub_array.each do |x|
+  # puts out each result
+  puts x
+  # end sub_array do loop
+  end
+  # end s.each do loop
+end
